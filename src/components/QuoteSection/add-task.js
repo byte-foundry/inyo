8d1b59bc1859f49861485e615df3f4fb138a972d
Julien Barret <ibarretj@gmail.com>
2018-10-15 10:35:26 +0200

    added an autocomplete for the task creation system

--- src/components/QuoteSection/add-task.js
+++ src/components/QuoteSection/add-task.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'react-emotion';
+import Autocomplete from 'react-autocomplete';
 import {H4, H5, FlexRow} from '../../utils/content';
 
 const AddTaskMain = styled('div')`
@@ -10,13 +11,56 @@ const AddTaskMain = styled('div')`
 const TaskComment = styled('textarea')`
 	width: 100%;
 `;
+const sampleTasks = [
+	'moodboard',
+	'benchmark',
+	'visual research',
+	'wireframes',
+	'development',
+	'review',
+];
 
 class AddTask extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedTask: '',
+		};
+	}
+
 	render() {
+		const {selectedTask} = this.state;
+
 		return (
 			<AddTaskMain>
 				<FlexRow justifyContent="space-between">
-					<input type="text" placeholder="autocompletion input" />
+					<Autocomplete
+						getItemValue={task => task}
+						items={sampleTasks}
+						shouldItemRender={(task, value) => task.includes(value)}
+						renderItem={(task, isHighlighted) => (
+							<div
+								style={{
+									background: isHighlighted
+										? 'lightgray'
+										: 'white',
+								}}
+							>
+								{task}
+							</div>
+						)}
+						value={selectedTask}
+						onChange={(e) => {
+							this.setState({
+								selectedTask: e.target.value,
+							});
+						}}
+						onSelect={(value) => {
+							this.setState({
+								selectedTask: value,
+							});
+						}}
+					/>
 					<input type="number" placeholder="1" />
 					<input type="text" placeholder="500€" />
 				</FlexRow>

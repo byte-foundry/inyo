32e686d8726fc851fbd87412680727f213b82729
FranzPoize <francois.poizat@gmail.com>
2018-11-06 15:51:44 +0100

    correcting merge mess up

--- src/components/CommentModal/index.js
+++ src/components/CommentModal/index.js
@@ -14,11 +14,46 @@ import {
 	ModalContainer,
 	ModalElem,
 	ModalCloseIcon,
-	ModalRow,
-	H2,
-} from '../../utils/content';
+	H4,
+	Button,
+	gray20,
+	primaryWhite,
+	gray30,
+	ErrorInput,
+	FlexRow
+} from "../../utils/content";
 
 import Comment from "../Comment";
+
+const CommentRow = styled("div")`
+	padding-left: 40px;
+	padding-right: 40px;
+	padding-top: 5px;
+	padding-bottom: 5px;
+`;
+
+const Comments = styled("div")`
+	max-height: 60vh;
+	overflow-y: auto;
+`;
+
+const ItemComment = styled("textarea")`
+	margin-top: 10px;
+	margin-left: 100px;
+	width: 100%;
+	background: ${primaryWhite};
+	border: 1px solid ${gray20};
+	padding: 15px 10px;
+	font-family: "Ligne";
+	color: ${gray30};
+	margin-bottom: 10px;
+`;
+
+const ActionButton = styled(Button)`
+	margin-bottom: 10px;
+	padding-left: 10px;
+	padding-right: 10px;
+`;
 
 
 class CommentModal extends Component {
@@ -53,10 +88,142 @@ class CommentModal extends Component {
 					return (
 						<ModalContainer>
 							<ModalElem>
-								<ModalRow>
-									<H2>Commentaires</H2>
-								</ModalRow>
-								<ModalRow>{commentsElem}</ModalRow>
+								<CommentRow>
+									<H4>{taskName}</H4>
+								</CommentRow>
+								<Comments>
+									<CommentRow>{commentsElem}</CommentRow>
+								</Comments>
+								<Mutation mutation={POST_COMMENT}>
+									{postComment => (
+										<Formik
+											initialValues={{
+												newComment: ""
+											}}
+											validationSchema={Yup.object().shape(
+												{
+													newComment: Yup.string().required(
+														"Requis"
+													)
+												}
+											)}
+											onSubmit={async (
+												values,
+												actions
+											) => {
+												actions.setSubmitting(false);
+												try {
+													postComment({
+														variables: {
+															itemId,
+															token: customerToken,
+															comment: {
+																text:
+																	values.newComment
+															}
+														},
+														update: (
+															cache,
+															{
+																data: {
+																	postComment
+																}
+															}
+														) => {
+															const data = cache.readQuery(
+																{
+																	query: GET_COMMENTS_BY_ITEM,
+																	variables: {
+																		itemId,
+																		token: customerToken
+																	}
+																}
+															);
+
+															data.itemComments =
+																postComment.comments;
+															try {
+																cache.writeQuery(
+																	{
+																		query: GET_COMMENTS_BY_ITEM,
+																		variables: {
+																			itemId,
+																			token: customerToken
+																		},
+																		data
+																	}
+																);
+																actions.resetForm();
+															} catch (e) {
+																console.log(e);
+															}
+														}
+													});
+												} catch (error) {
+													actions.setSubmitting(
+														false
+													);
+													actions.setErrors(error);
+													actions.setStatus({
+														msg:
+															"Something went wrong"
+													});
+												}
+											}}
+										>
+											{props => {
+												const {
+													touched,
+													errors,
+													handleSubmit,
+													setFieldValue,
+													values
+												} = props;
+
+												return (
+													<form
+														onSubmit={handleSubmit}
+													>
+														<FlexRow>
+															<ItemComment
+																placeholder="Votre commentaire"
+																value={
+																	values.newComment
+																}
+																name="newComment"
+																onChange={e =>
+																	setFieldValue(
+																		"newComment",
+																		e.target
+																			.value
+																	)
+																}
+															/>
+														</FlexRow>
+
+														{errors.comment &&
+															touched.comment && (
+																<ErrorInput>
+																	{
+																		errors.comment
+																	}
+																</ErrorInput>
+															)}
+														<FlexRow justifyContent="flex-end">
+															<ActionButton
+																theme="Primary"
+																size="Medium"
+																type="submit"
+															>
+																Envoyer
+															</ActionButton>
+														</FlexRow>
+													</form>
+												);
+											}}
+										</Formik>
+									)}
+								</Mutation>
 								<ModalCloseIcon
 									onClick={this.closeCommentModal}
 								>

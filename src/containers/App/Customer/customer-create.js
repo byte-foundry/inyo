28cac1fb6f9074a512f5805f8908e4389ced56d9
Julien Barret <ibarretj@gmail.com>
2018-11-07 15:35:31 +0100

    Fist draft - upload files

--- src/containers/App/Customer/customer-create.js
+++ src/containers/App/Customer/customer-create.js
@@ -3,8 +3,6 @@ import styled from 'react-emotion';
 
 import {H3} from '../../../utils/content';
 
-import CustomerForm from '../../../components/CustomerForm';
-
 const CreateCustomerMain = styled('div')``;
 
 class CreateCustomer extends Component {
@@ -12,7 +10,6 @@ class CreateCustomer extends Component {
 		return (
 			<CreateCustomerMain>
 				<H3>Create your customer</H3>
-				<CustomerForm />
 			</CreateCustomerMain>
 		);
 	}

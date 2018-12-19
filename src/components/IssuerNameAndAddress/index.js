16bac78b543d4f34d80b0212521b94c2b726600f
FranzPoize <francois.poizat@gmail.com>
2018-12-19 14:06:08 +0100

    fixing build after merge

--- src/components/IssuerNameAndAddress/index.js
+++ src/components/IssuerNameAndAddress/index.js
@@ -33,10 +33,6 @@ const ContactEmail = styled('div')`
 	color: ${primaryBlue};
 `;
 
-const ContactEmail = styled('div')`
-	color: ${primaryBlue};
-`;
-
 class IssuerNameAndAddress extends Component {
 	render() {
 		const {

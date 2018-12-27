b1b3ac8b0110de3e8c1227fd63f67cff30a579df
FranzPoize <francois.poizat@gmail.com>
2018-12-27 17:15:51 +0100

    removing remove section when an item in the section is finished

--- src/components/ProjectSection/index.js
+++ src/components/ProjectSection/index.js
@@ -73,21 +73,30 @@ class ProjectSection extends Component {
 					</SectionTitle>
 					<div>
 						<Mutation mutation={REMOVE_SECTION}>
-							{removeSection => (
-								<ProjectAction
-									theme="Link"
-									size="XSmall"
-									type="delete"
-									onClick={() => {
-										this.props.removeSection(
-											data.id,
-											removeSection,
-										);
-									}}
-								>
-									Supprimer la section
-								</ProjectAction>
-							)}
+							{(removeSection) => {
+								const display = data.items.reduce(
+									(acc, item) => acc && item.status !== 'FINISHED',
+									true,
+								);
+
+								return (
+									display && (
+										<ProjectAction
+											theme="Link"
+											size="XSmall"
+											type="delete"
+											onClick={() => {
+												this.props.removeSection(
+													data.id,
+													removeSection,
+												);
+											}}
+										>
+											Supprimer la section
+										</ProjectAction>
+									)
+								);
+							}}
 						</Mutation>
 					</div>
 				</FlexRow>

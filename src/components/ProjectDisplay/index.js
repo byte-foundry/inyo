aaa829df21924fe340f20e4d4b378eb10a5fbd17
Francois Poizat <francois.poizat@gmail.com>
2019-02-05 16:11:01 +0100

    git was a git on this one

--- src/components/ProjectDisplay/index.js
+++ src/components/ProjectDisplay/index.js
@@ -617,7 +617,7 @@ class ProjectDisplay extends Component {
 										<SideActions>
 											<CustomerIssuerContainer>
 												{customerToken
-													&& issuer.name && (
+													&& issuer && (
 													<IssuerNameAndAddress
 														issuer={issuer}
 													/>

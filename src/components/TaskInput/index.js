5a9a0f13bf1ee58f5cd24decff70863ea28386e5
Alexis Launay <yorunohikage@gmail.com>
2019-02-08 12:21:47 +0100

    Integrate input design and keyboard navigation

--- src/components/TaskInput/index.js
+++ src/components/TaskInput/index.js
@@ -1,18 +1,82 @@
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 import React, {useState, useRef} from 'react';
 import useOnClickOutside from 'use-onclickoutside';
 
 import TaskTypeDropdown from '../TaskTypeDropdown';
 
+const Container = styled('div')`
+	margin: 1rem 0;
+	font-size: 14px;
+`;
+
+const InputContainer = styled('div')`
+	display: flex;
+	align-items: center;
+	padding-left: 0.3rem;
+`;
+
+const Input = styled('input')`
+	flex: 1;
+	background-color: #f5f2fe;
+	border: none;
+	border-radius: 20px;
+	padding: 0.5rem 1.2rem 0.5rem 4rem;
+	margin-left: -2.1rem;
+	color: #5020ee;
+	font-size: 18px;
+	border: 1px solid transparent;
+	transition: all 400ms ease;
+
+	&::placeholder {
+		color: #888;
+		font-size: 14px;
+		font-style: italic;
+	}
+
+	&:focus {
+		outline: none;
+		outline: 0;
+		box-shadow: none;
+		background: #fff;
+		border: 1px solid #f5f2fe;
+		transition: all 400ms ease;
+	}
+`;
+
+const Icon = styled('div')`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #5020ee;
+	color: #fff;
+	border: 2px solid transparent;
+	border-radius: 50%;
+	min-width: 1.3rem;
+	min-height: 1.3rem;
+	width: 1.3rem;
+	height: 1.3rem;
+	z-index: 1;
+	transition: all 400ms ease;
+	cursor: pointer;
+
+	&:hover {
+		border: 2px solid #5020ee;
+		color: #5020ee;
+		background-color: #fff;
+		transition: all 400ms ease;
+	}
+`;
+
 const types = {
-	DEFAULT: <p>🛃</p>,
-	REMINDER: <p>⏰</p>,
-	VALIDATION: <p>👍</p>,
+	DEFAULT: <span>🛃</span>,
+	REMINDER: <span>⏰</span>,
+	VALIDATION: <span>👍</span>,
 };
 
 const TaskInput = ({onCreateProject, onCreateTask, defaultValue}) => {
 	const [value, setValue] = useState(defaultValue);
-	const [type, setType] = useState('DEFAULT');
+	const [type, setType] = useState(null);
 	const [focus, setFocus] = useState(false);
 	const ref = useRef();
 
@@ -21,46 +85,49 @@ const TaskInput = ({onCreateProject, onCreateTask, defaultValue}) => {
 	});
 
 	return (
-		<div ref={ref}>
-			{types[type] || types.DEFAULT}
-			<input
-				type="text"
-				onChange={e => setValue(e.target.value)}
-				value={value}
-				onFocus={() => setFocus(true)}
-				onKeyDown={(e) => {
-					if (e.key === 'ArrowUp') {
-						onCreateProject({
-							name: value,
-						});
-						setValue('');
+		<Container ref={ref}>
+			<InputContainer>
+				<Icon>{type ? types[type] || types.DEFAULT : '+'}</Icon>
+				<Input
+					type="text"
+					onChange={e => setValue(e.target.value)}
+					value={value}
+					onFocus={() => setFocus(true)}
+					onKeyDown={(e) => {
+						if (!value.startsWith('/')) {
+							if (e.key === 'ArrowUp') {
+								onCreateProject({
+									name: value,
+								});
+								setValue('');
+							}
+							else if (e.key === 'Enter') {
+								onCreateTask({
+									name: value,
+									type,
+								});
+								setValue('');
+							}
+						}
+					}}
+					placeholder={
+						focus
+							? 'Taper / pour choisir un type de tâche'
+							: 'Ajouter une tâche'
 					}
-					if (e.key === 'Enter') {
-						onCreateTask({
-							name: value,
-							type,
-						});
-						setValue('');
-					}
-				}}
-				placeholder={
-					focus
-						? 'Taper / pour choisir un type de tâche'
-						: 'Ajouter une tâche'
-				}
-			/>
+				/>
+			</InputContainer>
 			{value.startsWith('/')
 				&& focus && (
 				<TaskTypeDropdown
 					filter={value.substr(1)}
-					onSelectCommand={({type}) => {
-						console.log('selected', type);
-						setType(type);
+					onSelectCommand={({type: selectedType}) => {
+						setType(selectedType);
 						setValue('');
 					}}
 				/>
 			)}
-		</div>
+		</Container>
 	);
 };
 
--- src/components/TaskTypeDropdown/index.js
+++ src/components/TaskTypeDropdown/index.js
@@ -1,12 +1,29 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from '@emotion/styled';
 
 const Dropdown = styled('div')`
 	background: white;
-	box-shadow: 0 0 5px solid black;
+	box-shadow: 0 0 5px #e1e1e1;
 	position: absolute;
 	z-index: 1; /* do a portal instead */
+`;
+
+const List = styled('ul')`
+	display: flex;
+	flex-direction: column;
+	margin: 0;
+	padding: 0;
+	list-style: none;
+`;
+
+const ListItem = styled('li')`
+	cursor: pointer;
+	padding: 10px;
+
+	${props => props.focused && 'background: #e1e1e1;'} &:hover, &:focus {
+		background: #e1e1e1;
+	}
 `;
 
 const TaskTypeDropdown = ({filter, onSelectCommand}) => {
@@ -39,18 +56,56 @@ const TaskTypeDropdown = ({filter, onSelectCommand}) => {
 			|| description.toLocaleLowerCase().includes(lowercaseFilter),
 	);
 
+	const [focusedItemIndex, setFocusedItemIndex] = useState(0);
+
+	useEffect(() => {
+		const arrowNavigationListener = (e) => {
+			if (e.key === 'ArrowUp') {
+				setFocusedItemIndex(
+					(focusedItemIndex - 1 + types.length) % types.length,
+				);
+			}
+			else if (e.key === 'ArrowDown') {
+				setFocusedItemIndex(
+					(focusedItemIndex + 1 + types.length) % types.length,
+				);
+			}
+			else if (e.key === 'Enter') {
+				onSelectCommand(filteredTypes[focusedItemIndex]);
+			}
+		};
+
+		document.addEventListener('keydown', arrowNavigationListener);
+
+		return () => {
+			document.removeEventListener('keydown', arrowNavigationListener);
+		};
+	});
+
+	useEffect(
+		() => {
+			setFocusedItemIndex(0);
+		},
+		[filter],
+	);
+
 	return (
 		<Dropdown>
-			<ul>
+			<List>
 				{filteredTypes.map(({icon, name, description}, index) => (
-					<li onClick={e => onSelectCommand(filteredTypes[index])}>
+					<ListItem
+						tabIndex="0"
+						onMouseEnter={() => setFocusedItemIndex(index)}
+						onClick={() => onSelectCommand(filteredTypes[index])}
+						focused={index === focusedItemIndex}
+					>
 						{icon}
 						{name}
 						<br />
 						{description}
-					</li>
+					</ListItem>
 				))}
-			</ul>
+			</List>
 		</Dropdown>
 	);
 };

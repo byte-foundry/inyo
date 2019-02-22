533bd08a88e294551b4ef0bce6e32ed75342c78a
FranzPoize <francois.poizat@gmail.com>
2019-02-22 18:13:18 +0100

    new task menu

--- src/components/TasksList/index.js
+++ src/components/TasksList/index.js
@@ -17,9 +17,22 @@ function TasksList({items, projectId, customerId}) {
 	);
 }
 
-export default memo(TasksList, (prevProps, nextProps) => (
-	prevProps
+export default memo(
+	TasksList,
+	(prevProps, nextProps) => prevProps
 		&& prevProps.items.length === nextProps.items.length
+		&& prevProps.items.every(
+			(item, i) => item.name === nextProps.items[i].name
+				&& item.dueDate === nextProps.items[i].dueDate
+				&& item.unit === nextProps.items[i].unit
+				&& ((item.linkedCustomer === undefined
+					&& nextProps.items[i].linkedCustomer === undefined)
+					|| (item.linkedCustomer
+						&& nextProps.items[i].linkedCustomer
+						&& item.linkedCustomer.id
+							=== nextProps.items[i].linkedCustomer.id))
+				&& item.status === nextProps.items[i].status,
+		)
 		&& prevProps.projectId === nextProps.projectId
-		&& prevProps.customerId === nextProps.customerId
-));
+		&& prevProps.customerId === nextProps.customerId,
+);

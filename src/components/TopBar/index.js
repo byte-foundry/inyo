2f4097e6d6b6443bd3ece96e41d9c2ad43579ff3
Yannick Mathey <hello@yannickmathey.com>
2019-02-27 18:50:50 +0100

    simplier menu before a better one

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -8,6 +8,13 @@ import {
 	gray50,
 	H1,
 } from '../../utils/content';
+
+import {
+	primaryPurple,
+	primaryGrey,
+	primaryBlack,
+	accentGrey,
+} from '../../utils/new/design-system';
 import Logo from '../../utils/icons/inyo-topbar-logo.svg';
 
 export const TopBarLogo = styled('div')`
@@ -28,11 +35,11 @@ export const TopBarMenu = styled('div')`
 
 export const TopBarMenuLink = styled(Link)`
 	text-decoration: none;
-	margin-left: 10px;
+	margin-left: 2rem;
+	color: ${accentGrey};
 
-	&:before {
-		margin-right: 10px;
-		content: '\\2014';
+	&:hover {
+		color: ${primaryPurple};
 	}
 `;
 

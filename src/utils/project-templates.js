3b874a6527408b53a58b7f2187e7c4e316011db4
Yannick Mathey <hello@yannickmathey.com>
2019-03-01 13:18:16 +0100

    add template "I dont have any project"

--- src/utils/project-templates.js
+++ src/utils/project-templates.js
@@ -1,6 +1,101 @@
 export const templates = [
 	// eslint-disable-line import/prefer-default-export
 	{
+		name: 'PROSPECTION',
+		label: 'Trouver des clients',
+		sections: [
+			{
+				name: 'Utilisez vos expériences passées',
+				items: [
+					{
+						name: 'Recontacter ses anciens clients',
+						unit: 0.5,
+						description:
+							"Chercher de nouveaux clients est plus difficile que de convaincre d'anciens clients de faire de nouveau appel à vos services.",
+					},
+					{
+						name:
+							'Demander à vos anciens clients de vous recommander',
+						unit: 0.5,
+						description:
+							"Si vous ne l'avez pas encore fait, c'est le moment de contacter vos clients précédents pour que ceux-ci recommande vos compétences sur Linkedin et sur votre expérience commune.",
+					},
+				],
+			},
+			{
+				name: 'Augmentez votre visibilité',
+				items: [
+					{
+						name: 'Se référencer sur Google Business',
+						unit: 0.1,
+						description:
+							"Afin qu'on puisse vous trouver sur Google Maps et autres outils de Google, vous devez vous créer un profil pour optimiser vos chances de sortir dans une recherche Google. \n\n [S'enregistrer sur Google Business](https://www.google.fr/business)",
+					},
+					{
+						name: 'Se référencer sur le site des Pages Jaunes',
+						unit: 0.1,
+						description:
+							'Cela peut vous paraître étonnant, mais beaucoup de clients utilisent encore les Pages Jaunes pour trouver un professionnel.\n\n[Se référencer sur les Pages Jaunes](https://www.pagesjaunes.fr)',
+					},
+					{
+						name:
+							"S'inscrire sur des plateformes destinées aux freelances",
+						unit: 0.5,
+						description:
+							"Aujourd'hui de nombreuses entreprises passent par ces nouvelles plateformes. Nous vous encourageons à créer des profils sur les plus connues comme [Malt](https://www.malt.fr/profile) ou encore [Upwork](https://www.upwork.com).",
+					},
+					{
+						name: 'Créer un profil LinkedIn performant',
+						unit: 0.25,
+						description:
+							'Avoir un profil Linkedin est un minimum et autant faire en sorte que celui-ci soit performant: bien renseigner vos compétences, une description et une photo soignée et de liens vers quelques unes de vos références. Pensez à bien indiquer que vous êtes freelance, cela augmentera vos chances de ressortir dans les résultats de recherches.\n\nQuelques bon conseils sont à prendre sur [ce site.](https://www.codeur.com/blog/creer-profil-linkedin-puissant/)',
+					},
+					{
+						name: 'Mettre-à-jour ou créer son site internet',
+						unit: 2,
+						description:
+							"Vous vous en doutiez, un site est le moyen le plus complet et efficace de vous représenter. Si vous en possédez un, c'est le moment de le mettre à jour et d'en informer votre réseau (emails + réseaux sociaux). Si vous n'en avez pas encore, ne traînez plus et allez de ce pas en créer un avec les nombreux outils qui existent sur le web. Wordpress est le plus répandu et vous permettra de choisir un modèle parmi des milliers:\n\n[Créer un site vitrine sur Wordpress.com](https://wordpress.com)\n\nIl existe bien évidemment des alternatives: [Webflow,](https://webflow.com) [Squarespace,](https://fr.squarespace.com/) [Weebly,](https://www.weebly.com/) etc.",
+					},
+				],
+			},
+			{
+				name: 'Recherchez les offres en cours',
+				items: [
+					{
+						name: 'Créer une alerte Linkedin Jobs',
+						unit: 0.1,
+						description:
+							"Sur Linkedin il est possible de créer des recherches très précises et des alertes associées pour vous signaler une nouvelle offre de mission. Nous vous invitons fortement à créer votre propre alerte avec les mots clefs qui correspondent à votre profil.\n\n[Personnaliser ma recherche Linkedin](https://www.linkedin.com/jobs/search/?country=fr&pageNum=0&position=1&f_TP=1%2C2&keywords=Freelance&location=France)\n\nN'oubliez pas d'activer les alertes une fois cette recherche affinée.",
+					},
+					{
+						name: 'Créer une recherche avancée sur twitter',
+						unit: 0.1,
+						description:
+							"Ce n'est pas forcément la 1ère plateforme à laquelle on penserait, mais de nombreux clients postent leurs demandes sur Twitter. Ces offres peuvent parfois être moins sérieuses que sur Linkedin mais il est facile de se faire une idée rapidement.\n\n[Créer votre recherche avancée Twitter](https://twitter.com/search-advanced?lang=fr) et faîtes vous une idée par vous même (vous risquez d'être agréablement surpris)",
+					},
+				],
+			},
+			{
+				name: 'Communiquez sur votre recherche',
+				items: [
+					{
+						name:
+							'Annoncer sa disponibilité sur les réseaux sociaux',
+						unit: 0.1,
+						description:
+							"Votre réseau est votre allié. Postez sur les réseaux sociaux que vous êtes actuellement disponible et à la recherche de nouvelles missions. N'oubliez de mettre un lien vers vos références et votre spécialité!",
+					},
+					{
+						name: 'Faire appel à la communauté Inyo',
+						unit: 0.1,
+						description:
+							"La communauté Inyo grandit de jours en jours et compte des freelances aux nombreuses compétences: design graphique, développement, conception/rédaction, traduction, etc. N'hésitez pas à poster une annonce sur le [Slack](https://inyo-freelancers.slack.com) pour proposer vos services ou échanger vos bonnes pratiques!\n\n[Me connecter à la communauté Inyo](https://inyo-freelancers.slack.com)",
+					},
+				],
+			},
+		],
+	},
+	{
 		name: 'WEBSITE',
 		label: 'Site web',
 		sections: [

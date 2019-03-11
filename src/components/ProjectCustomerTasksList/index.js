4551a4f84d7e78d0a90bb325d74013d65f0d7793
Alexis Launay <yorunohikage@gmail.com>
2019-03-11 16:19:50 +0100

    Fix missing token in getProjectData query

--- src/components/ProjectCustomerTasksList/index.js
+++ src/components/ProjectCustomerTasksList/index.js
@@ -5,7 +5,7 @@ import {css} from '@emotion/core';
 
 import Task from '../TasksList/task';
 
-import {GET_PROJECT_DATA} from '../../utils/queries';
+import {GET_PROJECT_DATA_WITH_TOKEN} from '../../utils/queries';
 import {LayoutMainElem, primaryBlack} from '../../utils/new/design-system';
 import InlineEditable from '../InlineEditable';
 
@@ -39,13 +39,13 @@ const nameCss = css`
 function ProjectTasksList({
 	items, projectId, sectionId, customerToken,
 }) {
-	const {data: projectData, error} = useQuery(GET_PROJECT_DATA, {
-		variables: {projectId},
+	const {data, error} = useQuery(GET_PROJECT_DATA_WITH_TOKEN, {
+		variables: {projectId, token: customerToken},
 	});
 
 	if (error) throw error;
 
-	const {sections: sectionsInfos} = projectData.project;
+	const {sections: sectionsInfos} = data.project;
 
 	const sections = sectionsInfos.map((section) => {
 		const itemsInSection = items.filter(i => i.section.id === section.id);

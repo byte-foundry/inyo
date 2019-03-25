07e6225473a453f1d2265d7dfc1acad34340f610
FranzPoize <francois.poizat@gmail.com>
2019-03-25 17:04:39 +0100

    deadline input on the right

--- src/components/SidebarProjectInfos/index.js
+++ src/components/SidebarProjectInfos/index.js
@@ -440,7 +440,7 @@ const SidebarProjectInfos = ({
 								setEditDueDate(false);
 							}}
 							duration={project.total}
-							position="left"
+							position="right"
 						/>
 					)}
 				</DateContainer>

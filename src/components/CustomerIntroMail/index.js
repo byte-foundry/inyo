f344e208b631f01731ca9050ee52125dced3df5e
FranzPoize <francois.poizat@gmail.com>
2019-05-02 17:59:53 +0200

    fix bug when trying to create a customer

--- src/components/CustomerIntroMail/index.js
+++ src/components/CustomerIntroMail/index.js
@@ -56,16 +56,24 @@ const CustomerIntroMail = ({onDismiss, customer}) => {
 	const contentRef = useRef();
 	const [isCopied, setIsCopied] = useState(false);
 	const {
-		data: {
-			me: {
-				email,
-				settings: {assistantName},
-			},
-		},
+		data: {me},
 		loading,
 	} = useQuery(GET_USER_INFOS);
 
-	if (loading) return <Loading />;
+	if (loading) {
+		return (
+			<ModalContainer onDismiss={onDismiss}>
+				<ModalElem>
+					<Loading />
+				</ModalElem>
+			</ModalContainer>
+		);
+	}
+
+	const {
+		email,
+		settings: {assistantName},
+	} = me;
 
 	return (
 		<ModalContainer onDismiss={onDismiss}>

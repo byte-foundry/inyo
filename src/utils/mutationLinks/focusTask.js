68a3e85adeb977cb38d553011de8a8bb391133e9
Alexis Launay <yorunohikage@gmail.com>
2019-06-07 18:25:28 +0200

    Update cache for schedule

--- src/utils/mutationLinks/focusTask.js
+++ src/utils/mutationLinks/focusTask.js
@@ -12,4 +12,55 @@ export default {
 			reminders: [...cachedReminders],
 		};
 	},
+	getAllTasks: ({mutation, query}) => {
+		const {focusTask: task} = mutation.result.data;
+		const {tasks} = query.result.me;
+
+		const outdatedTask = tasks.find(item => item.id === task.id);
+
+		if (
+			outdatedTask.schedulePosition === task.schedulePosition
+			&& outdatedTask.scheduledFor === task.scheduledFor
+		) {
+			return {...query.result};
+		}
+
+		if (outdatedTask.scheduledFor !== task.scheduledFor) {
+			const outdatedList = tasks.filter(
+				item => item.scheduledFor === outdatedTask.scheduledFor
+					&& item.id !== outdatedTask.id,
+			);
+
+			outdatedList.sort(
+				(a, b) => a.schedulePosition - b.schedulePosition,
+			);
+			outdatedList.forEach((item, index) => {
+				item.schedulePosition = index;
+			});
+		}
+
+		const list = tasks.filter(
+			item => item.scheduledFor === task.scheduledFor
+				&& item.id !== outdatedTask.id,
+		);
+
+		list.sort((a, b) => a.schedulePosition - b.schedulePosition);
+		list.forEach((item, index) => {
+			item.schedulePosition
+				= index >= task.schedulePosition ? index + 1 : index;
+		});
+
+		return {
+			...query.result,
+			me: {
+				...query.result.me,
+				tasks: tasks.map((item) => {
+					if (outdatedTask.id === item.id) {
+						return task;
+					}
+					return item;
+				}),
+			},
+		};
+	},
 };

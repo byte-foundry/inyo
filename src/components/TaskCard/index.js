11aa4bb8be69efa0bac7397a5acd34c46b28a936
Alexis Launay <yorunohikage@gmail.com>
2019-06-10 14:36:29 +0200

    Add task validation and opening

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -1,5 +1,7 @@
 import React, {forwardRef} from 'react';
+import {withRouter} from 'react-router-dom';
 import styled from '@emotion/styled';
+import {useMutation} from 'react-apollo-hooks';
 
 import {isCustomerTask} from '../../utils/functions';
 import IconButton from '../../utils/new/components/IconButton';
@@ -8,6 +10,7 @@ import {
 	primaryBlack,
 	primaryGrey,
 } from '../../utils/new/design-system';
+import {FINISH_ITEM, UNFINISH_ITEM} from '../../utils/mutations';
 
 const TaskCardElem = styled('div')`
 	background: #fff;
@@ -18,6 +21,7 @@ const TaskCardElem = styled('div')`
 	font-size: 0.75rem;
 	display: grid;
 	grid-template-columns: 1fr auto;
+	cursor: pointer;
 
 	${props => props.isOver && 'border-top: 5px solid black;'}
 `;
@@ -33,35 +37,67 @@ const CardSubTitle = styled('span')`
 	color: ${primaryGrey};
 `;
 
-const TaskCard = forwardRef(
+const TaskCard = withRouter(
 	({
-		task, index, connectDragSource, connectDropTarget, ...rest
-	}, ref) => (
-		<TaskCardElem {...rest} ref={ref}>
-			{!isCustomerTask(task.type) && (
-				<IconButton
-					current={task.status === 'FINISHED'}
-					invert={task.status === 'FINISHED'}
-					style={{
-						gridColumnStart: '2',
-						gridRow: '1 / 3',
-					}}
-					icon="done"
-					size="tiny"
-				/>
-			)}
-			<CardTitle
-				style={{
-					gridColumn: isCustomerTask(task.type) ? '1 / 3' : '',
-				}}
+		task,
+		index,
+		connectDragSource,
+		connectDropTarget,
+		history,
+		location,
+		cardRef,
+		...rest
+	}) => {
+		const finishItem = useMutation(FINISH_ITEM);
+		const unfinishItem = useMutation(UNFINISH_ITEM);
+
+		return (
+			<TaskCardElem
+				{...rest}
+				ref={cardRef}
+				onClick={() => history.push({
+					pathname: `/app/dashboard/${task.id}`,
+					state: {prevSearch: location.search},
+				})
+				}
 			>
-				{task.name}
-			</CardTitle>
-			{task.linkedCustomer && (
-				<CardSubTitle>{task.linkedCustomer.name}</CardSubTitle>
-			)}
-		</TaskCardElem>
-	),
+				{!isCustomerTask(task.type) && (
+					<IconButton
+						current={task.status === 'FINISHED'}
+						invert={task.status === 'FINISHED'}
+						style={{
+							gridColumnStart: '2',
+							gridRow: '1 / 3',
+						}}
+						icon="done"
+						size="tiny"
+						onClick={(e) => {
+							e.stopPropagation();
+
+							if (task.status === 'FINISHED') {
+								unfinishItem({variables: {itemId: task.id}});
+							}
+							else {
+								finishItem({variables: {itemId: task.id}});
+							}
+						}}
+					/>
+				)}
+				<CardTitle
+					style={{
+						gridColumn: isCustomerTask(task.type) ? '1 / 3' : '',
+					}}
+				>
+					{task.name}
+				</CardTitle>
+				{task.linkedCustomer && (
+					<CardSubTitle>{task.linkedCustomer.name}</CardSubTitle>
+				)}
+			</TaskCardElem>
+		);
+	},
 );
 
-export default TaskCard;
+export default forwardRef((props, ref) => (
+	<TaskCard {...props} cardRef={ref} />
+));

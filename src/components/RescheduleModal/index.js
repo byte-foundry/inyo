3a04d109d7bebd28636d4c14eaf51448b6c2a663
Yannick Mathey <hello@yannickmathey.com>
2019-06-20 13:30:02 +0200

    add some space around tasks in poup

--- src/components/RescheduleModal/index.js
+++ src/components/RescheduleModal/index.js
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from '@emotion/styled';
 import {useMutation} from 'react-apollo-hooks';
 
 import Task from '../TasksList/task';
 import {ModalContainer, ModalElem} from '../../utils/content';
 import {P, Button} from '../../utils/new/design-system';
 import {UNFOCUS_TASK} from '../../utils/mutations';
+
+const WrapTasks = styled('div')`
+	margin: 1rem 0 2rem;
+`;
 
 const RescheduleModal = ({tasks}) => {
 	const unfocusTask = useMutation(UNFOCUS_TASK);
@@ -17,9 +22,11 @@ const RescheduleModal = ({tasks}) => {
 					Il semblerait qu'il y ait des tâches prévues les jours
 					précédents que vous n'avez pas validées ou déplacées.
 				</P>
-				{tasks.map(task => (
-					<Task item={task} baseUrl="dashboard" />
-				))}
+				<WrapTasks>
+					{tasks.map(task => (
+						<Task item={task} baseUrl="dashboard" />
+					))}
+				</WrapTasks>
 				<Button
 					onClick={() => tasks.forEach(task => unfocusTask({
 						variables: {

b79430c1faab34002d099f1b38b9fcabeaf00726
Alexis Launay <yorunohikage@gmail.com>
2019-07-18 18:10:37 +0200

    Link needing time it took tasks to tray

--- src/components/PendingActionsTray/index.js
+++ src/components/PendingActionsTray/index.js
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 import Portal from '@reach/portal';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useQuery} from 'react-apollo-hooks';
 
 import {BREAKPOINTS} from '../../utils/constants';
 import {P, primaryPurple, primaryWhite} from '../../utils/new/design-system';
+import {GET_ALL_TASKS} from '../../utils/queries';
 import MaterialIcon from '../MaterialIcon';
 
 const Tray = styled('div')`
@@ -15,7 +17,9 @@ const Tray = styled('div')`
 		0 3px 1px -2px rgba(0, 0, 0, 0.12);
 	transition: transform 400ms cubic-bezier(0.4, 0, 0.2, 1);
 	background: ${primaryWhite};
-	transform: ${props => !props.isOpen && 'translateY(calc(100% - 2rem))'};
+	transform: ${props => (!props.isVisible
+		? 'translateY(calc(100%))'
+		: !props.isOpen && 'translateY(calc(100% - 2rem))')};
 
 	@media (max-width: ${BREAKPOINTS}px) {
 		width: 100%;
@@ -50,13 +54,31 @@ const TitleBarIcon = styled('div')``;
 const Content = styled('div')``;
 
 const PendingActionsTray = () => {
-	const [isOpen, setIsOpen] = useState(true);
+	const [isOpen, setIsOpen] = useState(false);
+	const {
+		data: {
+			me: {tasks},
+		},
+	} = useQuery(GET_ALL_TASKS, {suspend: true});
+
+	const pendingTimeItTookTasks = tasks.filter(
+		task => task.timeItTook === null && task.status === 'FINISHED',
+	);
+	const isVisible = pendingTimeItTookTasks.length > 0;
+
+	useEffect(() => {
+		if (!isVisible) {
+			setIsOpen(false);
+		}
+	}, [isVisible]);
 
 	return (
 		<Portal>
-			<Tray isOpen={isOpen}>
+			<Tray isOpen={isOpen} isVisible={isVisible}>
 				<TitleBar onClick={() => setIsOpen(!isOpen)}>
-					<Title>Actions requises (2)</Title>
+					<Title>
+						Actions requises ({pendingTimeItTookTasks.length})
+					</Title>
 					<TitleBarIcon>
 						<MaterialIcon
 							icon={isOpen ? 'expand_more' : 'expand_less'}

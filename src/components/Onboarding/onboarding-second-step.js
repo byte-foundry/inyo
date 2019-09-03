1e26f9ecec9fc8537689a1c2c8bd756d03cdc1f0
Alexis Launay <yorunohikage@gmail.com>
2019-09-03 17:37:06 +0200

    Change onboarding pains to skills

--- src/components/Onboarding/onboarding-second-step.js
+++ src/components/Onboarding/onboarding-second-step.js
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
 import {Formik} from 'formik';
-import React, {Component} from 'react';
-import {Mutation} from 'react-apollo';
+import React from 'react';
+import {useMutation} from 'react-apollo-hooks';
 
 import fbt from '../../fbt/fbt.macro';
+import {BREAKPOINTS} from '../../utils/constants';
 import {
 	Button,
 	FlexColumn,
@@ -17,7 +18,6 @@ import {
 	primaryWhite,
 } from '../../utils/content';
 import {UPDATE_USER_CONSTANTS} from '../../utils/mutations';
-import {GET_USER_INFOS} from '../../utils/queries';
 import FormElem from '../FormElem';
 
 const OnboardingStep = styled('div')`
@@ -50,7 +50,6 @@ const DomainCards = styled(FlexRow)`
 `;
 
 const DomainCard = styled('div')`
-	width: 100%;
 	margin-bottom: 15px;
 	padding: 14px 16px 15px 16px;
 	color: ${props => (props.selected ? primaryWhite : gray80)};
@@ -60,267 +59,268 @@ const DomainCard = styled('div')`
 		border-color 0.3s ease;
 	cursor: pointer;
 	text-align: center;
+
+	box-sizing: border-box;
+	width: 32%;
+	margin-right: 2%;
+	:nth-child(3n) {
+		margin-right: 0;
+	}
+
+	@media (max-width: ${BREAKPOINTS}px) {
+		margin-right: 0;
+		width: 100%;
+	}
 `;
 
-class OnboardingSecondStep extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			painsExpressed: props.me.painsExpressed || [],
-		};
+const toggleSelectedItem = (oldSkills, item) => {
+	const skills = [...oldSkills];
+	const itemIndex = skills.findIndex(e => e === item);
+
+	if (itemIndex === -1) {
+		skills.push(item);
+	}
+	else {
+		skills.splice(itemIndex, 1);
 	}
 
-	toggleSelectedItems = (painsExpressed, item, setFieldValue) => {
-		const itemIndex = painsExpressed.findIndex(e => e === item);
+	return skills;
+};
 
-		if (itemIndex !== -1) {
-			painsExpressed.splice(itemIndex, 1);
-			setFieldValue('painsExpressed', painsExpressed);
+const OnboardingSecondStep = ({
+	me,
+	getNextStep,
+	getPreviousStep,
+	isFirstStep,
+}) => {
+	const [updateUser] = useMutation(UPDATE_USER_CONSTANTS);
 
-			return;
-		}
-		setFieldValue('painsExpressed', painsExpressed);
-		painsExpressed.push(item);
-		this.setState({painsExpressed});
-	};
+	const SKILLS = [
+		'PRINT_DESIGN',
+		'WEB_DESIGN',
+		'UX_DESIGN',
+		'UI_DESIGN',
+		'COPYWRITING',
+		'VIDEO',
+		'ACCOUNTING',
+		'PHOTOGRAPHY',
+		'MARKETING',
+		'FRONT_END_DEVELOPMENT',
+		'BACK_END_DEVELOPMENT',
+	];
 
-	render() {
-		const {
-			me, getNextStep, getPreviousStep, isFirstStep,
-		} = this.props;
-		const pains = [
-			<fbt project="inyo" desc="onboarding second step worries juggle">
-				Jongler entre plusieurs canaux de communications (Slack, emails,
-				etc.)
-			</fbt>,
-			<fbt project="inyo" desc="onboarding second step worries estimate">
-				Estimer correctement le temps et les tâches en amont du projet
-			</fbt>,
-			<fbt project="inyo" desc="onboarding second step worries organise">
-				Organiser et respecter mon programme de temps de travail
-			</fbt>,
-			<fbt project="inyo" desc="onboarding second step worries customer">
-				Relancer mes clients pour obtenir des validations
-			</fbt>,
-		];
+	return (
+		<OnboardingStep>
+			<StepSubtitle>
+				<fbt project="inyo" desc="onboarding second step title">
+					Quelles sont les compétences que vous maitrisez ?
+				</fbt>
+			</StepSubtitle>
+			<StepDescription>
+				<fbt project="inyo" desc="onboarding second step description">
+					Vous pouvez choisir plusieurs options
+				</fbt>
+			</StepDescription>
+			<Formik
+				initialValues={{
+					skills: me.skills || [],
+					other: '',
+				}}
+				validate={({skills, other}) => {
+					const errors = {};
 
-		return (
-			<OnboardingStep>
-				<StepSubtitle>
-					<fbt project="inyo" desc="onboarding second step title">
-						Qu'est-ce qui vous ennuie le plus au quotidien ?
-					</fbt>
-				</StepSubtitle>
-				<StepDescription>
-					<fbt
-						project="inyo"
-						desc="onboarding second step description"
-					>
-						Vous pouvez choisir plusieurs options
-					</fbt>
-				</StepDescription>
-				<Mutation mutation={UPDATE_USER_CONSTANTS}>
-					{updateUser => (
-						<Formik
-							initialValues={{
-								painsExpressed: me.painsExpressed || [],
-								other: '',
-							}}
-							validate={({painsExpressed, other}) => {
-								const errors = {};
+					if (
+						skills.find(
+							e => e
+								=== (
+									<fbt project="inyo" desc="other">
+										Autre
+									</fbt>
+								),
+						)
+						&& !other
+					) {
+						errors.other = (
+							<fbt project="inyo" desc="required">
+								Requis
+							</fbt>
+						);
+					}
 
-								if (
-									painsExpressed.find(
-										e => e
-											=== (
+					return errors;
+				}}
+				onSubmit={async (values, actions) => {
+					actions.setSubmitting(false);
+					window.Intercom('update', {
+						skills: values.skills,
+						otherSkill: values.otherSkill,
+					});
+					const newSkills = values.skills;
+
+					try {
+						await updateUser({
+							variables: {
+								skills: newSkills,
+								otherSkill: values.other,
+							},
+						});
+
+						getNextStep();
+					}
+					catch (error) {
+						actions.setSubmitting(false);
+						actions.setErrors(error);
+						actions.setStatus({
+							msg: (
+								<fbt project="inyo" desc="something went wrong">
+									Quelque chose s'est mal passé
+								</fbt>
+							),
+						});
+					}
+				}}
+			>
+				{(props) => {
+					const {
+						values,
+						errors,
+						setFieldValue,
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						touched,
+					} = props;
+
+					const otherSkillSelected = values.skills.find(
+						e => e
+							=== (
+								<fbt project="inyo" desc="other">
+									Autre
+								</fbt>
+							),
+					);
+
+					return (
+						<form onSubmit={handleSubmit}>
+							<DomainCards>
+								{SKILLS.map(skill => (
+									<DomainCard
+										selected={values.skills.find(
+											e => e === skill,
+										)}
+										onClick={() => {
+											setFieldValue(
+												'skills',
+												toggleSelectedItem(
+													values.skills,
+													skill,
+												),
+											);
+										}}
+									>
+										<fbt
+											project="inyo"
+											desc="onboarding second step skills"
+										>
+											<fbt:enum
+												enum-range={{
+													PRINT_DESIGN:
+														'Design Print',
+													WEB_DESIGN: 'Design web',
+													UX_DESIGN: 'Design UX',
+													UI_DESIGN: 'Design UI',
+													COPYWRITING: 'Copywriting',
+													VIDEO: 'Vidéo',
+													ACCOUNTING: 'Comptabilité',
+													PHOTOGRAPHY: 'Photographie',
+													MARKETING: 'Marketing',
+													FRONT_END_DEVELOPMENT:
+														'Dev front end',
+													BACK_END_DEVELOPMENT:
+														'Dev back end',
+												}}
+												value={skill}
+											/>
+										</fbt>
+									</DomainCard>
+								))}
+								<DomainCard
+									selected={otherSkillSelected}
+									onClick={() => {
+										setFieldValue(
+											'skills',
+											toggleSelectedItem(
+												values.skills,
 												<fbt
 													project="inyo"
 													desc="other"
 												>
 													Autre
-												</fbt>
+												</fbt>,
 											),
-									)
-									&& !other
-								) {
-									errors.other = (
-										<fbt project="inyo" desc="required">
-											Requis
-										</fbt>
-									);
-								}
-
-								return errors;
-							}}
-							onSubmit={async (values, actions) => {
-								actions.setSubmitting(false);
-								window.Intercom('update', {
-									painsExpressed: values.painsExpressed,
-									otherPain: values.otherPain,
-								});
-								const newPainsExpressed = values.painsExpressed;
-
-								try {
-									await updateUser({
-										variables: {
-											painsExpressed: newPainsExpressed,
-											otherPain: values.other,
-										},
-									});
-
-									getNextStep();
-								}
-								catch (error) {
-									actions.setSubmitting(false);
-									actions.setErrors(error);
-									actions.setStatus({
-										msg: (
+										);
+									}}
+								>
+									<fbt project="inyo" desc="other">
+										Autre
+									</fbt>
+								</DomainCard>
+								{otherSkillSelected && (
+									<FormElem
+										label={
 											<fbt
 												project="inyo"
-												desc="something went wrong"
+												desc="onboarding second step more information"
 											>
-												Quelque chose s'est mal passé
+												Donnez nous un exemple :)
 											</fbt>
-										),
-									});
-								}
-							}}
-						>
-							{(props) => {
-								const {
-									values,
-									errors,
-									setFieldValue,
-									handleChange,
-									handleBlur,
-									handleSubmit,
-									touched,
-								} = props;
-
-								return (
-									<form onSubmit={handleSubmit}>
-										<DomainCards>
-											{pains.map(pain => (
-												<DomainCard
-													selected={values.painsExpressed.find(
-														e => e === pain,
-													)}
-													onClick={() => {
-														this.toggleSelectedItems(
-															values.painsExpressed,
-															pain,
-															setFieldValue,
-														);
-													}}
-												>
-													{pain}
-												</DomainCard>
-											))}
-											<DomainCard
-												selected={values.painsExpressed.find(
-													e => e
-														=== (
-															<fbt
-																project="inyo"
-																desc="other"
-															>
-																Autre
-															</fbt>
-														),
-												)}
-												onClick={() => {
-													this.toggleSelectedItems(
-														values.painsExpressed,
-														<fbt
-															project="inyo"
-															desc="other"
-														>
-															Autre
-														</fbt>,
-														setFieldValue,
-													);
-												}}
-											>
-												<fbt
-													project="inyo"
-													desc="other"
-												>
-													Autre
-												</fbt>
-											</DomainCard>
-											{values.painsExpressed.find(
-												e => e
-													=== (
-														<fbt
-															project="inyo"
-															desc="other"
-														>
-															Autre
-														</fbt>
-													),
-											) && (
-												<FormElem
-													label={
-														<fbt
-															project="inyo"
-															desc="onboarding second step more information"
-														>
-															Donnez nous un
-															exemple :)
-														</fbt>
-													}
-													errors={errors}
-													required
-													values={values}
-													type="text"
-													touched={touched}
-													name="other"
-													id="other"
-													handleBlur={handleBlur}
-													handleChange={handleChange}
-												/>
-											)}
-										</DomainCards>
-										<ActionButtons>
-											<ActionButton
-												theme="Primary"
-												size="Medium"
-												type="submit"
-											>
-												<fbt
-													project="inyo"
-													desc="onboarding second step confirm"
-												>
-													Continuer
-												</fbt>
-											</ActionButton>
-											{!isFirstStep && (
-												<ActionButton
-													theme="Link"
-													size="XSmall"
-													onClick={() => {
-														getPreviousStep();
-													}}
-												>
-													{'< '}
-													<fbt
-														project="inyo"
-														desc="back"
-													>
-														Retour
-													</fbt>
-												</ActionButton>
-											)}
-										</ActionButtons>
-									</form>
-								);
-							}}
-						</Formik>
-					)}
-				</Mutation>
-			</OnboardingStep>
-		);
-	}
-}
+										}
+										errors={errors}
+										required
+										values={values}
+										type="text"
+										touched={touched}
+										name="other"
+										id="other"
+										handleBlur={handleBlur}
+										handleChange={handleChange}
+									/>
+								)}
+							</DomainCards>
+							<ActionButtons>
+								<ActionButton
+									theme="Primary"
+									size="Medium"
+									type="submit"
+								>
+									<fbt
+										project="inyo"
+										desc="onboarding second step confirm"
+									>
+										Continuer
+									</fbt>
+								</ActionButton>
+								{!isFirstStep && (
+									<ActionButton
+										theme="Link"
+										size="XSmall"
+										onClick={() => {
+											getPreviousStep();
+										}}
+									>
+										{'< '}
+										<fbt project="inyo" desc="back">
+											Retour
+										</fbt>
+									</ActionButton>
+								)}
+							</ActionButtons>
+						</form>
+					);
+				}}
+			</Formik>
+		</OnboardingStep>
+	);
+};
 
 export default OnboardingSecondStep;

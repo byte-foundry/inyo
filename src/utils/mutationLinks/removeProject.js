9aaf5bdcb83853ccffd151bb41568733ad6f8182
Francois Poizat <francois.poizat@gmail.com>
2019-11-07 14:50:12 +0100

    added a getAllTasks and getAllTasksShort on removeProject

--- src/utils/mutationLinks/removeProject.js
+++ src/utils/mutationLinks/removeProject.js
@@ -1,4 +1,21 @@
+import produce from 'immer';
+
+const getAllTaskMutation = ({mutation, query}) => {
+	const removedProject = mutation.result.data.removeProject;
+	const tasks = removedProject.sections.reduce(
+		(acc, section) => acc.concat(section.items.map(item => item.id)),
+		[],
+	);
+
+	return produce(query.result, (draft) => {
+		// remove old
+		draft.me.tasks = draft.me.tasks.filter(t => !tasks.includes(t.id));
+	});
+};
+
 export default {
+	getAllTasksShort: args => getAllTaskMutation(args),
+	getAllTasks: args => getAllTaskMutation(args),
 	getAllProjects({mutation, query}) {
 		const {me} = query.result;
 

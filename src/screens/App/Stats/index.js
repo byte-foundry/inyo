87f1b6971a39fcc47ddbff28cc2c824deb7720ad
Yannick Mathey <hello@yannickmathey.com>
2019-11-20 21:12:38 +0100

    WIP add a heatmap on page stats

--- src/screens/App/Stats/index.js
+++ src/screens/App/Stats/index.js
@@ -1,17 +1,18 @@
-import styled from '@emotion/styled/macro';
-import moment from 'moment';
-import React, {useCallback} from 'react';
-import {Link, withRouter} from 'react-router-dom';
+import styled from "@emotion/styled/macro";
+import moment from "moment";
+import React, { useCallback } from "react";
+import { Link, withRouter } from "react-router-dom";
+import CalendarHeatmap from "react-calendar-heatmap";
 
-import ArianneThread, {ArianneElem} from '../../../components/ArianneThread';
-import HelpAndTooltip from '../../../components/HelpAndTooltip';
-import MaterialIcon from '../../../components/MaterialIcon';
-import SingleBarChart from '../../../components/SingleBarChart';
-import TasksProgressBar from '../../../components/TasksProgressBar';
-import fbt from '../../../fbt/fbt.macro';
-import {useQuery} from '../../../utils/apollo-hooks';
-import {BREAKPOINTS} from '../../../utils/constants';
-import {formatName} from '../../../utils/functions';
+import ArianneThread, { ArianneElem } from "../../../components/ArianneThread";
+import HelpAndTooltip from "../../../components/HelpAndTooltip";
+import MaterialIcon from "../../../components/MaterialIcon";
+import SingleBarChart from "../../../components/SingleBarChart";
+import TasksProgressBar from "../../../components/TasksProgressBar";
+import fbt from "../../../fbt/fbt.macro";
+import { useQuery } from "../../../utils/apollo-hooks";
+import { BREAKPOINTS } from "../../../utils/constants";
+import { formatName } from "../../../utils/functions";
 import {
 	A,
 	accentGrey,
@@ -23,7 +24,7 @@ import {
 import {GET_ALL_TASKS_STATS} from '../../../utils/queries';
 import useUserInfos from '../../../utils/useUserInfos';
 
-const Container = styled('div')`
+const Container = styled("div")`
 	width: 980px;
 	margin: 3.5rem auto;
 	min-height: 100vh;
@@ -33,7 +34,7 @@ const Container = styled('div')`
 	}
 `;
 
-const Cards = styled('div')`
+const Cards = styled("div")`
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
 	grid-column-gap: 20px;
@@ -44,7 +45,7 @@ const Cards = styled('div')`
 	}
 `;
 
-const Card = styled('div')`
+const Card = styled("div")`
 	background-color: ${mediumGrey};
 	border-radius: 4px;
 	padding: 1rem;
@@ -59,7 +60,7 @@ const Number = styled(P)`
 	margin: 0;
 `;
 
-const TimeSelectContainer = styled('div')`
+const TimeSelectContainer = styled("div")`
 	display: flex;
 	align-items: center;
 	width: 200px;
@@ -70,7 +71,7 @@ const TimeSelect = styled(ArianneElem)`
 	width: 100%;
 `;
 
-const MetaHeading = styled('div')`
+const MetaHeading = styled("div")`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
@@ -85,14 +86,18 @@ const PageSubHeading = styled(SubHeading)`
 	margin: 2rem 0;
 `;
 
-const Section = styled('div')`
+const Section = styled("div")`
 	margin-bottom: 5rem;
 `;
 
-const Stats = ({history, location}) => {
+const CalendarHeatmapWrap = styled("div")`
+	max-height: 200px;
+`;
+
+const Stats = ({ history, location }) => {
 	const {
 		data: {
-			me: {id, tasks},
+			me: { id, tasks }
 		},
 		error,
 	} = useQuery(GET_ALL_TASKS_STATS, {suspend: true});
@@ -103,22 +108,22 @@ const Stats = ({history, location}) => {
 
 	const query = new URLSearchParams(location.search);
 
-	const since = parseInt(query.get('since'), 10) || 30;
-	const projectId = query.get('projectId');
-	const tags = query.getAll('tags');
-	const linkedCustomerId = query.get('customerId');
+	const since = parseInt(query.get("since"), 10) || 30;
+	const projectId = query.get("projectId");
+	const tags = query.getAll("tags");
+	const linkedCustomerId = query.get("customerId");
 
 	if (error) throw error;
 
 	const setSince = useCallback(
-		(value) => {
+		value => {
 			const newQuery = new URLSearchParams(query);
 
-			newQuery.set('since', value);
+			newQuery.set("since", value);
 
 			history.push(`/app/stats?${newQuery.toString()}`);
 		},
-		[query],
+		[query]
 	);
 
 	const setProjectSelected = useCallback(
@@ -126,94 +131,94 @@ const Stats = ({history, location}) => {
 			const newQuery = new URLSearchParams(query);
 
 			if (selected) {
-				const {value: selectedProjectId} = selected;
+				const { value: selectedProjectId } = selected;
 
-				newQuery.set('projectId', selectedProjectId);
-			}
-			else if (newQuery.has('projectId')) {
-				newQuery.delete('projectId');
+				newQuery.set("projectId", selectedProjectId);
+			} else if (newQuery.has("projectId")) {
+				newQuery.delete("projectId");
 			}
 
 			if (removeCustomer) {
-				newQuery.delete('customerId');
+				newQuery.delete("customerId");
 			}
 
 			history.push(`/app/stats?${newQuery.toString()}`);
 		},
-		[query, history],
+		[query, history]
 	);
 
 	const setCustomerSelected = useCallback(
-		(selected) => {
+		selected => {
 			const newQuery = new URLSearchParams(query);
 
 			if (selected) {
-				const {value: selectedCustomerId} = selected;
+				const { value: selectedCustomerId } = selected;
 
-				newQuery.set('customerId', selectedCustomerId);
-			}
-			else if (newQuery.has('customerId')) {
-				newQuery.delete('customerId');
+				newQuery.set("customerId", selectedCustomerId);
+			} else if (newQuery.has("customerId")) {
+				newQuery.delete("customerId");
 			}
 
-			if (newQuery.has('projectId')) {
-				newQuery.delete('projectId');
+			if (newQuery.has("projectId")) {
+				newQuery.delete("projectId");
 			}
 
 			history.push(`/app/stats?${newQuery.toString()}`);
 		},
-		[query, history],
+		[query, history]
 	);
 
 	const setTagSelected = useCallback(
-		(selected) => {
+		selected => {
 			const newQuery = new URLSearchParams(query);
 
 			if (selected) {
-				newQuery.delete('tags');
-				selected.forEach(tag => newQuery.append('tags', tag.value));
+				newQuery.delete("tags");
+				selected.forEach(tag => newQuery.append("tags", tag.value));
 			}
 
 			history.push(`/app/stats?${newQuery.toString()}`);
 		},
-		[query, history],
+		[query, history]
 	);
 
 	const filteredTasks = tasks.filter(
-		task => moment(task.createdAt).isSameOrAfter(
-			moment().subtract(since, 'days'),
-		)
-			&& (!linkedCustomerId
-				|| ((task.linkedCustomer
-					&& task.linkedCustomer.id === linkedCustomerId)
-					|| (task.section
-						&& task.section.project.customer
-						&& task.section.project.customer.id
-							=== linkedCustomerId)))
-			&& (!task.section
-				|| task.section.project.status !== 'REMOVED'
-				|| projectId)
-			&& (!projectId
-				|| (task.section && task.section.project.id === projectId))
-			&& tags.every(tag => task.tags.some(taskTag => taskTag.id === tag))
-			&& !(task.assignee && task.assignee.id !== id),
+		task =>
+			moment(task.createdAt).isSameOrAfter(
+				moment().subtract(since, "days")
+			) &&
+			(!linkedCustomerId ||
+				((task.linkedCustomer &&
+					task.linkedCustomer.id === linkedCustomerId) ||
+					(task.section &&
+						task.section.project.customer &&
+						task.section.project.customer.id ===
+							linkedCustomerId))) &&
+			(!task.section ||
+				task.section.project.status !== "REMOVED" ||
+				projectId) &&
+			(!projectId ||
+				(task.section && task.section.project.id === projectId)) &&
+			tags.every(tag => task.tags.some(taskTag => taskTag.id === tag)) &&
+			!(task.assignee && task.assignee.id !== id)
 	);
 
 	const customers = {};
 
 	let totalTime = 0;
 
-	tasks.forEach((task) => {
+	tasks.forEach(task => {
 		if (
-			task.status !== 'FINISHED'
-			|| moment(task.createdAt).isBefore(moment().subtract(since, 'days'))
-			|| (task.section && task.section.project.status === 'REMOVED')
-			|| (task.assignee && task.assignee.id !== id)
-		) return;
+			task.status !== "FINISHED" ||
+			moment(task.createdAt).isBefore(moment().subtract(since, "days")) ||
+			(task.section && task.section.project.status === "REMOVED") ||
+			(task.assignee && task.assignee.id !== id)
+		)
+			return;
 
-		const customer
-			= task.linkedCustomer
-			|| (task.section && task.section.project.customer);
+		const customer =
+			task.linkedCustomer ||
+			(task.section && task.section.project.customer);
 
 		if (!customer) return;
 
@@ -221,12 +226,12 @@ const Stats = ({history, location}) => {
 			value: 0,
 			label: `${customer.name} (${formatName(
 				customer.firstName,
-				customer.lastName,
-			)})`,
+				customer.lastName
+			)})`
 		};
 
-		const time
-			= typeof task.timeItTook === 'number' ? task.timeItTook : task.unit;
+		const time =
+			typeof task.timeItTook === "number" ? task.timeItTook : task.unit;
 
 		customers[customer.id].value += time;
 		totalTime += time;
@@ -236,31 +241,54 @@ const Stats = ({history, location}) => {
 		([key, obj]) => ({
 			id: key,
 			label: obj.label,
-			value: (obj.value / totalTime) * 100,
-		}),
+			value: (obj.value / totalTime) * 100
+		})
 	);
 
 	const reminders = filteredTasks
-		.filter(t => t.status === 'FINISHED')
+		.filter(t => t.status === "FINISHED")
 		.map(task => task.reminders)
 		.flat();
+
+	let today = moment();
+
+	const randomValues = getRange(200).map(index => {
+		return {
+			date: shiftDate(today, -index),
+			count: getRandomInt(1, 3)
+		};
+	});
+
+	function shiftDate(date, numDays) {
+		const newDate = new Date(date);
+		newDate.setDate(newDate.getDate() + numDays);
+		return newDate;
+	}
+
+	function getRange(count) {
+		return Array.from({ length: count }, (_, i) => i);
+	}
+
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
 	return (
 		<Container>
 			<MetaHeading>
-				<Heading style={{marginBottom: 0}}>
+				<Heading style={{ marginBottom: 0 }}>
 					<fbt project="inyo" desc="Statistics">
 						Statistiques
 					</fbt>
 				</Heading>
 				<TimeSelectContainer>
 					<fbt project="inyo" desc="Show">
-						Afficher{' '}
+						Afficher{" "}
 						<fbt:param name="timespan">
 							{
 								<TimeSelect
 									selectedId={since}
-									onChange={({value}) => setSince(value)}
+									onChange={({ value }) => setSince(value)}
 									list={[
 										{
 											name: (
@@ -271,7 +299,7 @@ const Stats = ({history, location}) => {
 													les 7 derniers jours
 												</fbt>
 											),
-											id: 7,
+											id: 7
 										},
 										{
 											name: (
@@ -282,7 +310,7 @@ const Stats = ({history, location}) => {
 													les 30 derniers jours
 												</fbt>
 											),
-											id: 30,
+											id: 30
 										},
 										{
 											name: (
@@ -293,7 +321,7 @@ const Stats = ({history, location}) => {
 													les 3 derniers mois
 												</fbt>
 											),
-											id: 90,
+											id: 90
 										},
 										{
 											name: (
@@ -304,8 +332,8 @@ const Stats = ({history, location}) => {
 													les 6 derniers mois
 												</fbt>
 											),
-											id: 180,
-										},
+											id: 180
+										}
 									]}
 								/>
 							}
@@ -313,6 +341,24 @@ const Stats = ({history, location}) => {
 					</fbt>
 				</TimeSelectContainer>
 			</MetaHeading>
+
+			<CalendarHeatmapWrap>
+				<CalendarHeatmap
+					startDate={shiftDate(today, -since)}
+					endDate={shiftDate(today, +7)}
+					values={randomValues}
+					classForValue={value => {
+						if (!value) {
+							return "color-empty";
+						}
+						return `color-github-${value.count}`;
+					}}
+					showWeekdayLabels={false}
+					onClick={value =>
+						alert(`Clicked on value with count: ${value.count}`)
+					}
+				/>
+			</CalendarHeatmapWrap>
 
 			<Section>
 				<PageSubHeading>
@@ -368,8 +414,8 @@ const Stats = ({history, location}) => {
 					showCompletionPercentage={false}
 					project={{
 						sections: filteredTasks
-							.filter(t => t.status === 'FINISHED')
-							.map(t => ({items: t})),
+							.filter(t => t.status === "FINISHED")
+							.map(t => ({ items: t }))
 					}}
 				/>
 				<Cards>
@@ -391,10 +437,11 @@ const Stats = ({history, location}) => {
 						<Number>
 							{(
 								filteredTasks
-									.filter(t => t.status === 'FINISHED')
+									.filter(t => t.status === "FINISHED")
 									.reduce(
-										(total, {timeItTook}) => total + timeItTook,
-										0,
+										(total, { timeItTook }) =>
+											total + timeItTook,
+										0
 									) * workingTime
 							).toFixed(0)}
 							h
@@ -418,10 +465,10 @@ const Stats = ({history, location}) => {
 						<Number>
 							{(
 								filteredTasks
-									.filter(t => t.status === 'FINISHED')
+									.filter(t => t.status === "FINISHED")
 									.reduce(
-										(total, {unit}) => total + unit,
-										0,
+										(total, { unit }) => total + unit,
+										0
 									) * workingTime
 							).toFixed(0)}
 							h
@@ -435,7 +482,7 @@ const Stats = ({history, location}) => {
 							<HelpAndTooltip icon="help">
 								<fbt desc="reminders sent tooltip">
 									<p>
-										Nombre de rappels que votre{' '}
+										Nombre de rappels que votre{" "}
 										<i>Smart Assistant</i> a envoyé selon
 										les filtres et la période sélectionnés.
 									</p>
@@ -445,7 +492,7 @@ const Stats = ({history, location}) => {
 						<Number>
 							{
 								reminders.filter(
-									reminder => reminder.status === 'SENT',
+									reminder => reminder.status === "SENT"
 								).length
 							}
 						</Number>
@@ -459,7 +506,7 @@ const Stats = ({history, location}) => {
 								<fbt desc="Gained time tooltip">
 									<p>
 										Temps gagné grâce à l'utilisation
-										d'Inyo, du fait des actions de votre{' '}
+										d'Inyo, du fait des actions de votre{" "}
 										<i>Smart Assistant</i>.
 									</p>
 								</fbt>
@@ -469,11 +516,11 @@ const Stats = ({history, location}) => {
 							{moment
 								.duration(
 									reminders.filter(
-										reminder => reminder.status === 'SENT',
+										reminder => reminder.status === "SENT"
 									).length * 15,
-									'minutes',
+									"minutes"
 								)
-								.format('h_mm_')}
+								.format("h_mm_")}
 						</Number>
 					</Card>
 					<Card>
@@ -501,10 +548,10 @@ const Stats = ({history, location}) => {
 							<HelpAndTooltip icon="help">
 								<fbt desc="estimated time tooltip">
 									<p>
-										Valeur de votre travail selon{' '}
+										Valeur de votre travail selon{" "}
 										<Link to="/app/account#settings">
 											votre TJM
-										</Link>{' '}
+										</Link>{" "}
 										et les filtres et période sélectionnés.
 									</p>
 								</fbt>
@@ -512,15 +559,16 @@ const Stats = ({history, location}) => {
 						</SubHeading>
 						<Number>
 							{new Intl.NumberFormat(language, {
-								style: 'currency',
-								currency: language === 'fr' ? 'EUR' : 'USD',
+								style: "currency",
+								currency: language === "fr" ? "EUR" : "USD"
 							}).format(
 								filteredTasks
-									.filter(t => t.status === 'FINISHED')
+									.filter(t => t.status === "FINISHED")
 									.reduce(
-										(total, {timeItTook}) => total + timeItTook,
-										0,
-									) * defaultDailyPrice,
+										(total, { timeItTook }) =>
+											total + timeItTook,
+										0
+									) * defaultDailyPrice
 							)}
 						</Number>
 					</Card>
